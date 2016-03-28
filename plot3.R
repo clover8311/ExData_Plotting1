eb64a8f49ad91e90558f44dc2fcfaf4cf2bdd15e
clover8311 <yanze0823@gmail.com>
2016-03-28 00:57:20 -0700

    Delete the plot3 to upload a new one.

--- plot3.R
+++ plot3.R
@@ -15,3 +15,4 @@ lines(datetime, Sub_metering_2, type="l", col="red")
 lines(datetime, Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off();
+
